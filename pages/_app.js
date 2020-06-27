0c5b6015653b8d897570d6d93b3cb3861e06bd40
vattcarter7 <vattcarter@gmail.com>
2020-06-27 16:56:41 +0700

    created pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
+import Navbar from '../components/shared/Navbar';
+import Hero from '../components/shared/Hero';
+import App from 'next/app';
+
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className='portfolio-app'>
+      <Navbar />
+      {pageProps.appData}
+      {Component.name === 'Home' && <Hero />}
+      <div className='container'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 };
+
+MyApp.getInitialProps = () => {
+  return { pageProps: { appData: 'hello _App Component' } };
+};
+
 export default MyApp;
